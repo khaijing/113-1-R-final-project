34782995e881fb376b1247a4e643ca413474284d
khaijing <kcjing1234@gmail.com>
2024-11-20 06:51:16 +0000

    new

--- Final.R
+++ Final.R
@@ -1,1 +1,12 @@
-aaaaa
+install.packages("tidyverse")
+
+install.packages("remotes")
+
+remotes::install_github("tpemartin/ntpuR")
+
+library(ntpuR)
+setup_github_personal_access_token
+
+library(readr)
+data <- read_csv("政府行政機關辦公日曆表_export.csv")
+ 
