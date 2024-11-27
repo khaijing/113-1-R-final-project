ab37db46c821d75f138ffe2dfba0f12b10bd2768
khaijing <kcjing1234@gmail.com>
2024-11-27 14:02:52 +0800

    Update Final.R

--- Final.R
+++ Final.R
@@ -10,3 +10,22 @@ setup_github_personal_access_token
 library(readr)
 data <- read_csv("政府行政機關辦公日曆表_export.csv")
  
+
+library(tidyverse)
+
+data <- data %>%
+  rename(
+    日期 = date,
+    年份 = year,
+    名稱 = name,
+    是否為假日 = isholiday,
+    假日類別 = holidaycategory,
+    描述 = description
+  )
+
+print(data)
+
+data <- data %>%
+  mutate(年份 = 年份 - 1911)
+
+print(data)
