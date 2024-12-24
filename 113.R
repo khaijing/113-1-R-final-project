32edd1701a990867cb4ef8dbcf87e1360b2a88f6
econmtlin <kcjing1234@gmail.com>
2024-12-24 08:46:59 +0000

    final program

--- 113.R
+++ 113.R
@@ -1,45 +1,19 @@
 # 讀取 CSV 文件
 export <- read_csv("臺北市企業營運總部分布圖.csv")
 
-USEDATE <- as.character(USEDATE)
-
-library(stringr)
-
-# 假設 USEDATE 是資料框中的一列
-USEDATE <- c("1130909-1131010", "1121231-1130101") # 示例數據
-
-# 將年份和日期分開處理
-western_USEDATE <- str_replace_all(USEDATE, "(\\d{3})(\\d{4})-(\\d{3})(\\d{4})", function(x) {
-  year1 <- as.numeric(substr(x, 1, 3)) + 1911
-  year2 <- as.numeric(substr(x, 11, 13)) + 1911
-  sprintf("%d%s-%d%s", year1, substr(x, 4, 7), year2, substr(x, 14, 17))
-})
-
-# 查看結果
-western_USEDATE
-
-glimpse(export)
-
-library(dplyr)
-
-# 確保 CATEGORY 是字符型，並處理 NA
-filtered_export <- export |> 
-  filter(!is.na(CATEGORY) & str_detect(as.character(CATEGORY), "民生化工類"))
-
-glimpse(filtered_export)
-
-library(dplyr)
-library(stringr)
+export <- export |> 
+  mutate(
+    western_year = str_sub(USEDATE, 1, 3) |> 
+      as.numeric() + 1911, # 提取前3位年份並加上1911
+    western_date = str_replace(USEDATE, "^\\d{3}", as.character(western_year)) # 用西元年取代台灣年
+  )|> 
+select(-western_year) # 如果不需要保留 `western_year` 欄位，可以刪除
+print(export)
 
 export <- export |> 
-  mutate(extracted_area = str_extract(ADDR, "(?<=市).*?區"))
+  mutate(
+    district = str_extract(ADDR, "(?<=市).+?區") # 匹配市和區之間的文字
+  )
 
-glimpse(export)
-
-export <- export |> 
-  select(-city_area)
-
-glimpse(export)
-
-glimpse(export[1:3,])
+glimpse(export[1:2,])
 
