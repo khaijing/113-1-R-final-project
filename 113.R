f68110d32ab4efb11124abe4995abd64272c3d66
econmtlin <kcjing1234@gmail.com>
2024-12-25 06:34:44 +0000

    final exam

--- 113.R
+++ 113.R
@@ -1,5 +1,7 @@
 export <- readr::read_csv("臺北市企業營運總部分布圖.csv")
 
+view(export)
+
 # 假設資料框名稱為 export
 export <- export |>
   dplyr::mutate(
@@ -31,5 +33,25 @@ district_data <- export |>
   dplyr::filter(district == "中山區")
 
 glimpse(district_data)
+
+district_count <- export |> 
+  dplyr::filter(district %in% c("中山區", "內湖區", "大同區", "信義區", "北投區", "士林區", "大安區", "南港區", "松山區", "中正區")) |> 
+  dplyr::count(district) |> 
+  dplyr::rename(數量 = n)
+
+glimpse(district_count)
+
+library(ggplot2)
+
+district_count <- export |> 
+  dplyr::filter(district %in% c("中山區", "內湖區", "大同區", "信義區", "北投區", "士林區", "大安區", "南港區", "松山區", "中正區")) |> 
+  dplyr::count(district) |> 
+  dplyr::rename(數量 = n)
+
+ggplot(district_count, aes(x = district, y = 數量, fill = district)) +
+  geom_bar(stat = "identity") +
+  labs(title = "各區域的數量分佈", x = "區域", y = "數量") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 glimpse(export[1:2,])
